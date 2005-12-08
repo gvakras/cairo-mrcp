ecfbde665d32c6071e1be05cf7e3b4d6c878e8af
ngodfredsen <ngodfredsen@2108db3d-1710-0410-b8b3-d782175cc310>
2005-12-08 01:27:07 +0000

    add support for starting input timers and no-input-timeout event generation, move waitForResult threading from RTPRecogChannel to SphinxRecEngine

--- trunk/cairo/java/com/onomatopia/cairo/server/recog/RTPRecogChannel.java
+++ trunk/cairo/java/com/onomatopia/cairo/server/recog/RTPRecogChannel.java
@@ -25,47 +25,52 @@ package com.onomatopia.cairo.server.recog;
 import com.onomatopia.cairo.server.recog.sphinx.SphinxRecEngine;
 import com.onomatopia.cairo.server.resource.ResourceUnavailableException;
 import com.onomatopia.cairo.server.rtp.RTPStreamReplicator;
+import com.onomatopia.cairo.util.jmf.ProcessorStarter;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.concurrent.TimeoutException;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import javax.media.ConfigureCompleteEvent;
-import javax.media.ControllerEvent;
-import javax.media.ControllerListener;
 import javax.media.Processor;
-import javax.media.RealizeCompleteEvent;
-import javax.media.StartEvent;
-import javax.media.control.TrackControl;
 import javax.media.protocol.ContentDescriptor;
-import javax.media.protocol.DataSource;
-import javax.media.protocol.FileTypeDescriptor;
 import javax.media.protocol.PushBufferDataSource;
-import javax.media.protocol.PushBufferStream;
 import javax.speech.recognition.GrammarException;
 
 import org.apache.commons.lang.Validate;
 import org.apache.commons.pool.ObjectPool;
-import org.mrcp4j.MrcpEventName;
-import org.mrcp4j.MrcpRequestState;
-import org.mrcp4j.message.MrcpEvent;
-import org.mrcp4j.server.MrcpSession;
+import org.apache.log4j.Logger;
 
 /**
  * TODOC
  * @author Niels Godfredsen {@literal <}<a href="mailto:ngodfredsen@users.sourceforge.net">ngodfredsen@users.sourceforge.net</a>{@literal >}
  *
  */
-public class RTPRecogChannel implements ControllerListener {
+public class RTPRecogChannel {
 
+    public static final short WAITING_FOR_SPEECH = 0;
+    public static final short SPEECH_IN_PROGRESS = 1;
+    public static final short COMPLETE = 2;
+
+    static Logger _logger = Logger.getLogger(RTPRecogChannel.class);
+
+    /**
+     * Content descriptor for raw audio content.
+     */
     private static final ContentDescriptor CONTENT_DESCRIPTOR_RAW =
         new ContentDescriptor(ContentDescriptor.RAW);
 
-    ObjectPool _recEnginePool;
+    private /*static*/ Timer _timer = new Timer();
+
+    private ObjectPool _recEnginePool;
     private RTPStreamReplicator _replicator;
 
+    RecogListener _recogListener;
+    SphinxRecEngine _recEngine = null;
+    TimerTask _noInputTimeoutTask;
+
     private Processor _processor;
+
+    volatile short _state = COMPLETE;
 
     /**
      * TODOC
@@ -82,123 +87,188 @@ public class RTPRecogChannel implements ControllerListener {
 
     /**
      * TODOC
-     * @param listener 
-     * @param startInputTimers 
-     * @param grammarName 
-     * @throws IOException 
-     * @throws IllegalStateException 
-     * @throws ResourceUnavailableException 
-     * @throws GrammarException 
+     * @param listener
+     * @param grammarLocation
+     * @param noInputTimeout
+     * @throws IllegalStateException
+     * @throws IOException
+     * @throws ResourceUnavailableException
+     * @throws GrammarException
      */
-    public void recognize(RecogListener listener, boolean startInputTimers, GrammarLocation grammarLocation)
+    public synchronized void recognize(RecogListener listener, GrammarLocation grammarLocation, long noInputTimeout)
       throws IllegalStateException, IOException, ResourceUnavailableException, GrammarException {
 
         if (_processor != null) {
             throw new IllegalStateException("Recognition already in progress!");
         }
-        
+
         _processor = _replicator.createRealizedProcessor(CONTENT_DESCRIPTOR_RAW, 10000); // TODO: specify audio format
 
         PushBufferDataSource dataSource = (PushBufferDataSource) _processor.getDataOutput();
         if (dataSource == null) {
             throw new IOException("Processor.getDataOutput() returned null!");
         }
-        
-        SphinxRecEngine recEngine = null;
+
         try {
-            recEngine = (SphinxRecEngine) _recEnginePool.borrowObject();
+            _logger.debug("Borrowing recognition engine from object pool...");
+            _recEngine = (SphinxRecEngine) _recEnginePool.borrowObject();
         } catch (Exception e) {
-            e.printStackTrace();
-            _processor.close();
-            _processor = null;
+            _logger.debug(e, e);
+            closeProcessor();
             throw new ResourceUnavailableException("All rec engines are in use!", e);
             // TODO: wait for availability...?
         }
+
+        _recogListener = new Listener(listener);
         
         try {
-            System.out.println("Starting recognition...");
-            recEngine.loadJSGF(grammarLocation);
-            recEngine.startRecognition(dataSource);
-            _processor.addControllerListener(this);
+            _logger.debug("Loading grammar...");
+            _recEngine.loadJSGF(grammarLocation);
+
+            _logger.debug("Starting recognition...");
+            _state = WAITING_FOR_SPEECH;
+            _recEngine.startRecognition(dataSource, _recogListener);
+
+            _processor.addControllerListener(new ProcessorStarter());
             _processor.start();
-            //dataSource.connect();
-            //dataSource.start();
-            // TODO: hang on to recEngine for recognition completion, etc.
-            new RecogThread(recEngine, listener).start();
+
+            _recEngine.startRecogThread();
+
+            if (noInputTimeout > 0) {
+                startInputTimers(noInputTimeout);
+            }
+
         } catch (GrammarException e) {
-            close(recEngine);
+            closeProcessor();
             throw e;
         } catch (IOException e) {
-            close(recEngine);
+            closeProcessor();
             throw e;
         }
     }
-    
-    private void close(SphinxRecEngine recEngine) {
+
+    /**
+     * Starts the input timers which trigger no-input-timeout if speech has not started after the specified time.
+     * @param noInputTimeout the amount of time to wait, in milliseconds, before triggering a no-input-timeout. 
+     * @return <@code true> if input timers were started or <@code false> if speech has already started.
+     * @throws IllegalStateException if recognition is not in progress or if the input timers have already been started.
+     */
+    public synchronized boolean startInputTimers(long noInputTimeout) throws IllegalStateException {
+        if (noInputTimeout <= 0) {
+            throw new IllegalArgumentException("Illegal value for no-input-timeout: " + noInputTimeout);
+        }
+        if (_processor == null) {
+            throw new IllegalStateException("Recognition not in progress!");
+        }
+        if (_noInputTimeoutTask != null) {
+            throw new IllegalStateException("InputTimer already started!");
+        }
+
+        boolean startInputTimers = (_state == WAITING_FOR_SPEECH); 
+        if (startInputTimers) {
+            _noInputTimeoutTask = new NoInputTimeoutTask();
+            _timer.schedule(_noInputTimeoutTask, noInputTimeout);
+        }
+
+        return startInputTimers;
+    }
+
+    synchronized void closeProcessor() {
         if (_processor != null) {
+          _logger.debug("Closing processor...");
             _processor.close();
             _processor = null;
         }
-        if (recEngine != null) {
-            try {
-                _recEnginePool.returnObject(recEngine);
-            } catch (Exception e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-        }
-    }
-    
-    // TODO: move threading to RecEngine
-    private class RecogThread extends Thread {
-
-        private SphinxRecEngine _recEngine;
-        private RecogListener _listener;
-
-        private RecogThread(SphinxRecEngine recEngine, RecogListener listener) {
-            _recEngine = recEngine;
-            _listener = listener;
-        }
-        
-        /* (non-Javadoc)
-         * @see java.lang.Runnable#run()
-         */
-        @Override
-        public void run() {
-            System.out.println("RecogThread waiting for result...");
-            RecognitionResult result = _recEngine.waitForResult(_listener);
-            System.out.println("**************************************************************");
-            System.out.println("RecogThread got result: " + result.toString());
-            System.out.println("**************************************************************");
+        if (_recEngine != null) {
             try {
                 _recEnginePool.returnObject(_recEngine);
             } catch (Exception e) {
                 // TODO Auto-generated catch block
-                e.printStackTrace();
+                _logger.debug(e, e);
             }
-            _listener.recognitionComplete();
+            _recEngine = null;
         }
     }
 
-    /* (non-Javadoc)
-     * @see javax.media.ControllerListener#controllerUpdate(javax.media.ControllerEvent)
-     */
-    public void controllerUpdate(ControllerEvent event) {
-        System.out.println("RTPRecogChannel: ControllerEvent received: " + event);
-        try {
-            if (event instanceof StartEvent) {
-                Processor processor = (Processor) event.getSourceController();
-                DataSource dataSource = processor.getDataOutput();
-                System.out.println("Starting data source...");
-                dataSource.connect();
-                dataSource.start();
-            //} else if (event instanceof StopEvent) { //EndOfMediaEvent) {
-                //event.getSourceController().close();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private class NoInputTimeoutTask extends TimerTask {
 
+        /* (non-Javadoc)
+         * @see java.util.TimerTask#run()
+         */
+        @Override
+        public void run() {
+            synchronized (RTPRecogChannel.this) {
+                _noInputTimeoutTask = null;
+                if (_state == WAITING_FOR_SPEECH) {
+                    _state = COMPLETE;
+                    closeProcessor();
+                    if (_recogListener != null) {
+                        _recogListener.noInputTimeout();
+                    }
+                }
+            }
+        }
+        
     }
 
+    private class Listener extends RecogListenerDecorator {
+
+        /**
+         * TODOC
+         * @param recogListener
+         */
+        public Listener(RecogListener recogListener) {
+            super(recogListener);
+        }
+
+        /* (non-Javadoc)
+         * @see com.onomatopia.cairo.server.recog.RecogListener#speechStarted()
+         */
+        @Override
+        public void speechStarted() {
+            synchronized (RTPRecogChannel.this) {
+                if (_state == WAITING_FOR_SPEECH) {
+                    _state = SPEECH_IN_PROGRESS;
+                }
+                if (_noInputTimeoutTask != null) {
+                    _noInputTimeoutTask.cancel();
+                }
+            }
+            super.speechStarted();
+        }
+
+        /* (non-Javadoc)
+         * @see com.onomatopia.cairo.server.recog.RecogListener#recognitionComplete()
+         */
+        @Override
+        public void recognitionComplete(RecognitionResult result) {
+            boolean doit = false;
+            synchronized (RTPRecogChannel.this) {
+                if (_state == SPEECH_IN_PROGRESS) {
+                    _state = COMPLETE;
+                    doit = true;
+                }
+            }
+            if (doit) {
+                closeProcessor();
+                super.recognitionComplete(result);
+            }
+        }
+
+//        public void noInputTimeout() {
+//            boolean doit = false;
+//            synchronized (RTPRecogChannel.this) {
+//                doit = _speechStarted;
+//                _speechStarted = false;
+//            }
+//            try {
+//                _recEnginePool.returnObject(_recEngine);
+//            } catch (Exception e) {
+//                // TODO Auto-generated catch block
+//                _logger.debug(e, e);
+//            }
+//            super.noInputTimeout();
+//        }
+
+    }
 }
