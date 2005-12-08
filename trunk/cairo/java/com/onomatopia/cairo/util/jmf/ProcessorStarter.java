5093f394333cc3efbe03cdc27e29554fc185ddb8
ngodfredsen <ngodfredsen@2108db3d-1710-0410-b8b3-d782175cc310>
2005-12-08 00:54:35 +0000

    utility class for starting up a JMF processor

---
+++ trunk/cairo/java/com/onomatopia/cairo/util/jmf/ProcessorStarter.java
@@ -0,0 +1,49 @@
+/**
+ * TODOC
+ */
+package com.onomatopia.cairo.util.jmf;
+
+import java.io.IOException;
+
+import javax.media.ControllerEvent;
+import javax.media.ControllerListener;
+import javax.media.Processor;
+import javax.media.StartEvent;
+import javax.media.protocol.DataSource;
+
+import org.apache.log4j.Logger;
+
+/**
+ * TODOC
+ *
+ * @author Niels Godfredsen {@literal <}<a href="mailto:ngodfredsen@users.sourceforge.net">ngodfredsen@users.sourceforge.net</a>{@literal >}
+ */
+public class ProcessorStarter implements ControllerListener {
+
+    private static Logger _logger = Logger.getLogger(ProcessorStarter.class);
+
+    /* (non-Javadoc)
+     * @see javax.media.ControllerListener#controllerUpdate(javax.media.ControllerEvent)
+     */
+    public void controllerUpdate(ControllerEvent event) {
+        if (_logger.isDebugEnabled()) {
+            _logger.debug("ControllerEvent received: " + event);
+        }
+
+        try {
+            if (event instanceof StartEvent) {
+                Processor processor = (Processor) event.getSourceController();
+                DataSource dataSource = processor.getDataOutput();
+                _logger.debug("Starting data source...");
+                dataSource.connect();
+                dataSource.start();
+            //} else if (event instanceof StopEvent) { //EndOfMediaEvent) {
+                //event.getSourceController().close();
+            }
+        } catch (IOException e) {
+            _logger.warn(e, e);
+        }
+
+    }
+
+}
