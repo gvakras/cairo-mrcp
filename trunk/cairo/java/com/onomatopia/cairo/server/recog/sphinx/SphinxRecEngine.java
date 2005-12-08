582442fb55f58b59b1397451afc58c3509e8c00d
ngodfredsen <ngodfredsen@2108db3d-1710-0410-b8b3-d782175cc310>
2005-12-08 01:29:26 +0000

    move waitForResult threading from RTPRecogChannel to SphinxRecEngine

--- trunk/cairo/java/com/onomatopia/cairo/server/recog/sphinx/SphinxRecEngine.java
+++ trunk/cairo/java/com/onomatopia/cairo/server/recog/sphinx/SphinxRecEngine.java
@@ -24,9 +24,14 @@ package com.onomatopia.cairo.server.recog.sphinx;
 
 import com.onomatopia.cairo.server.recog.GrammarLocation;
 import com.onomatopia.cairo.server.recog.RecogListener;
+import com.onomatopia.cairo.server.recog.RecogListenerDecorator;
 import com.onomatopia.cairo.server.recog.RecognitionResult;
 import com.onomatopia.cairo.server.recog.SpeechEventListener;
+import com.onomatopia.cairo.server.rtp.PBDSReplicator;
+import com.onomatopia.cairo.util.jmf.ProcessorStarter;
+import com.onomatopia.cairo.util.pool.AbstractPoolableObject;
 
+import java.awt.Toolkit;
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
@@ -57,19 +62,24 @@ import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 import edu.cmu.sphinx.util.props.PropertyException;
 
+import org.apache.log4j.Logger;
+
 /**
  * TODOC
  * @author Niels Godfredsen {@literal <}<a href="mailto:ngodfredsen@users.sourceforge.net">ngodfredsen@users.sourceforge.net</a>{@literal >}
  *
  */
-public class SphinxRecEngine implements SpeechEventListener {
+public class SphinxRecEngine extends AbstractPoolableObject implements SpeechEventListener {
+
+    static Logger _logger = Logger.getLogger(SphinxRecEngine.class);
+    private static Toolkit _toolkit = _logger.isTraceEnabled()? Toolkit.getDefaultToolkit() : null;
 
     private Recognizer _recognizer;
     private JSGFGrammar _jsgfGrammar;
     private RawAudioProcessor _rawAudioProcessor;
 
     private RawAudioTransferHandler _rawAudioTransferHandler;
-    private RecogListener _recogListener;
+    RecogListener _recogListener;
 
     protected SphinxRecEngine(ConfigurationManager cm)
       throws IOException, PropertyException, InstantiationException {
@@ -98,17 +108,29 @@ public class SphinxRecEngine implements SpeechEventListener {
         }
     }
 
-    public synchronized void activate() /*throws Exception*/ {
-        System.out.println("SphinxRecEngine activating...");
+    /* (non-Javadoc)
+     * @see com.onomatopia.cairo.util.pool.PoolableObject#activate()
+     */
+    @Override
+    public synchronized void activate() {
+        _logger.debug("SphinxRecEngine activating...");
     }
 
+    /* (non-Javadoc)
+     * @see com.onomatopia.cairo.util.pool.PoolableObject#passivate()
+     */
+    @Override
     public synchronized void passivate() {
-        System.out.println("SphinxRecEngine passivating...");
+        _logger.debug("SphinxRecEngine passivating...");
         stopProcessing();
+        _recogListener = null;
     }
 
+    /**
+     * TODOC
+     */
     public synchronized void stopProcessing() {
-        System.out.println("SphinxRecEngine stopping processing...");
+        _logger.debug("SphinxRecEngine stopping processing...");
         if (_rawAudioTransferHandler != null) {
             _rawAudioTransferHandler.stopProcessing();
             _rawAudioTransferHandler = null;
@@ -116,24 +138,36 @@ public class SphinxRecEngine implements SpeechEventListener {
         // TODO: should wait to set this until after run thread completes (i.e. recognizer is cleared)
     }
 
+    /**
+     * TODOC
+     * @param grammarLocation
+     * @throws IOException
+     * @throws GrammarException
+     */
     public synchronized void loadJSGF(GrammarLocation grammarLocation) throws IOException, GrammarException {
         _jsgfGrammar.setBaseURL(grammarLocation.getBaseURL());
         try {
             _jsgfGrammar.loadJSGF(grammarLocation.getGrammarName());
-            System.out.println("loadJSGF(): completed successfully.");
+            _logger.debug("loadJSGF(): completed successfully.");
         } catch (com.sun.speech.engine.recognition.TokenMgrError e) {
-            System.out.println("loadJSGF(): encountered exception: " + e.getClass().getName()); // com.sun.speech.engine.recognition.TokenMgrError!!!
+            _logger.debug("loadJSGF(): encountered exception: " + e.getClass().getName(), e); // com.sun.speech.engine.recognition.TokenMgrError!!!
             String message = e.getMessage();
             /*if (message.indexOf("speech") < 0) {
                 throw e;
             }*/
             // else assume caused by GrammarException
             // TODO: edu.cmu.sphinx.jsapi.JSGFGrammar.loadJSGF() should be updated not to swallow GrammarException
-            e.printStackTrace();
             throw new GrammarException(message);
         }
     }
 
+    /**
+     * TODOC
+     * @param text
+     * @param ruleName
+     * @return
+     * @throws GrammarException
+     */
     public synchronized RuleParse parse(String text, String ruleName) throws GrammarException {
         if (_rawAudioTransferHandler != null) {
             throw new IllegalStateException("Recognition already in progress!");
@@ -146,9 +180,10 @@ public class SphinxRecEngine implements SpeechEventListener {
     /**
      * TODOC
      * @param dataSource
+     * @param listener 
      * @throws UnsupportedEncodingException 
      */
-    public synchronized void startRecognition(PushBufferDataSource dataSource)
+    public synchronized void startRecognition(PushBufferDataSource dataSource, RecogListener listener)
       throws UnsupportedEncodingException {
 
         if (_rawAudioTransferHandler != null) {
@@ -160,6 +195,7 @@ public class SphinxRecEngine implements SpeechEventListener {
             throw new IllegalArgumentException(
                 "Rec engine can handle only single stream datasources, # of streams: " + streams);
         }
+        _logger.debug("starting recognition");
         try {
             _rawAudioTransferHandler = new RawAudioTransferHandler(_rawAudioProcessor);
             _rawAudioTransferHandler.startProcessing(streams[0]);
@@ -167,24 +203,25 @@ public class SphinxRecEngine implements SpeechEventListener {
             _rawAudioTransferHandler = null;
             throw e;
         }
+
+        _recogListener = listener;
     }
 
-    /**
-     * TODOC
-     * @param listener 
-     * @return
-     */
-    public RecognitionResult waitForResult(RecogListener listener) {
-        _recogListener = listener;
+    // TODO: rename method
+    public void startRecogThread() {
+        new RecogThread().start();
+    }
+
+    private RecognitionResult waitForResult() {
         Result result = _recognizer.recognize();
         stopProcessing();
         if (result != null) {
             Result result2clear = _recognizer.recognize();
             if (result2clear != null) {
-                System.out.println("waitForResult(): result2clear not null!");
+                _logger.debug("waitForResult(): result2clear not null!");
             }
         } else {
-            System.out.println("waitForResult(): got null result from recognizer!");
+            _logger.debug("waitForResult(): got null result from recognizer!");
             return null;
         }
         return new RecognitionResult(result);
@@ -194,8 +231,19 @@ public class SphinxRecEngine implements SpeechEventListener {
      * @see com.onomatopia.cairo.server.recog.SpeechEventListener#speechStarted()
      */
     public void speechStarted() {
-        if (_recogListener != null) {
-            _recogListener.speechStarted();
+        if (_toolkit != null) {
+            _toolkit.beep();
+        }
+
+        RecogListener recogListener = null;
+        synchronized (this) {
+            recogListener = _recogListener; 
+        }
+
+        if (recogListener == null) {
+            _logger.debug("speechStarted(): _recogListener is null!");
+        } else {
+            recogListener.speechStarted();
         }
     }
 
@@ -203,90 +251,174 @@ public class SphinxRecEngine implements SpeechEventListener {
      * @see com.onomatopia.cairo.server.recog.SpeechEventListener#speechEnded()
      */
     public void speechEnded() {
-        // TODO Auto-generated method stub
+        if (_toolkit != null) {
+            _toolkit.beep();
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
+    // inner classes
+    ///////////////////////////////////////////////////////////////////////////
+
+    private class RecogThread extends Thread {
         
-    }
-
-    private static MediaLocator MICROPHONE = new MediaLocator("dsound://");
-    private static AudioFormat[] PREFERRED_MEDIA_FORMATS = {SourceAudioFormat.PREFERRED_MEDIA_FORMAT};
-    private static final ContentDescriptor CONTENT_DESCRIPTOR_RAW = new ContentDescriptor(ContentDescriptor.RAW);
-
-    public static void main(String[] args) throws Exception {
-        URL url;
-        if (args.length > 0) {
-            url = new File(args[0]).toURL();
-        } else {
-            url = SphinxRecEngine.class.getResource("/config/sphinx-config.xml");
-        }
-        
-        if (url == null) {
-            throw new RuntimeException("Sphinx config file not found!");
-        }
-
-        System.out.println("Loading...");
-        ConfigurationManager cm = new ConfigurationManager(url);
-        SphinxRecEngine engine = new SphinxRecEngine(cm);
-
-        for (int i=0; i < 12; i++) {
-            System.out.println(engine._jsgfGrammar.getRandomSentence());
-        }
-
-        RecognitionResult result;
-        while (true) {
-            result = doRecognize(engine);
-        }
-
-//        RuleParse ruleParse = engine.parse("", "main");
-
-
-        //System.exit(0);
-    }
-
-    private static RecognitionResult doRecognize(SphinxRecEngine engine)
-      throws NoDataSourceException, IOException, NoProcessorException, CannotRealizeException {
-
-        engine.activate();
-
-        DataSource dataSource = Manager.createDataSource(MICROPHONE);
-        ProcessorModel pm = new ProcessorModel(dataSource, PREFERRED_MEDIA_FORMATS, CONTENT_DESCRIPTOR_RAW);
-        Processor processor = Manager.createRealizedProcessor(pm);
-        processor.addControllerListener(new Listener());
-
-        PushBufferDataSource pbds = (PushBufferDataSource) processor.getDataOutput();
-        engine.startRecognition(pbds);
-        processor.start();
-        System.out.println("Performing recognition...");
-        RecognitionResult result = engine.waitForResult(null);
-        System.out.println("**************************************************\n" +
-                           "result: " + result + "\n" +
-                           "**************************************************");
-
-        engine.passivate();
-
-        return result;
-    }
-    
-    private static class Listener implements ControllerListener {
-
         /* (non-Javadoc)
-         * @see javax.media.ControllerListener#controllerUpdate(javax.media.ControllerEvent)
+         * @see java.lang.Runnable#run()
          */
-        public void controllerUpdate(ControllerEvent event) {
-            System.out.println("RTPRecogChannel: ControllerEvent received: " + event);
-            try {
-                if (event instanceof StartEvent) {
-                    Processor processor = (Processor) event.getSourceController();
-                    DataSource dataSource = processor.getDataOutput();
-                    System.out.println("Starting data source...");
-                    dataSource.connect();
-                    dataSource.start();
-                //} else if (event instanceof StopEvent) { //EndOfMediaEvent) {
-                    //event.getSourceController().close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        @Override
+        public void run() {
+            _logger.debug("RecogThread waiting for result...");
+
+            RecognitionResult result = SphinxRecEngine.this.waitForResult();
+
+            if (_logger.isDebugEnabled()) {
+                StringBuilder sb = new StringBuilder();
+                sb.append("\n**************************************************************");
+                sb.append("\nRecogThread got result: ").append(result);
+                sb.append("\n**************************************************************");
+                _logger.debug(sb);
+            }
+            
+            RecogListener recogListener = null;
+            synchronized (SphinxRecEngine.this) {
+                recogListener = _recogListener;
             }
 
+            if (recogListener == null) {
+                _logger.debug("RecogThread.run(): _recogListener is null!");
+            } else {
+                recogListener.recognitionComplete(result);
+            }
+        }
+    }
+
+    /**
+     * Provides main method for testing SphinxRecEngine in standalone mode using the microphone for input.
+     */
+    public static class Test extends RecogListenerDecorator {
+
+        private static MediaLocator MICROPHONE = new MediaLocator("dsound://");
+        private static AudioFormat[] PREFERRED_MEDIA_FORMATS = {SourceAudioFormat.PREFERRED_MEDIA_FORMAT};
+        private static final ContentDescriptor CONTENT_DESCRIPTOR_RAW = new ContentDescriptor(ContentDescriptor.RAW);
+
+        private SphinxRecEngine _engine;
+        private RecognitionResult _result;
+        private PBDSReplicator _replicator;
+
+        public Test(SphinxRecEngine engine)
+          throws NoProcessorException, NoDataSourceException, CannotRealizeException, IOException {
+            super(null);
+            _engine = engine;
+            _replicator = createMicrophoneReplicator();
+        }
+
+        /* (non-Javadoc)
+         * @see com.onomatopia.cairo.server.recog.RecogListener#recognitionComplete(com.onomatopia.cairo.server.recog.RecognitionResult)
+         */
+        @Override
+        public synchronized void recognitionComplete(RecognitionResult result) {
+            _result = result;
+            this.notify();
+        }
+
+        public RecognitionResult doRecognize() throws IOException, NoProcessorException, CannotRealizeException,
+                InterruptedException {
+
+            _result = null;
+            _engine.activate();
+
+            Processor processor = createReplicatedProcessor();
+            processor.addControllerListener(new ProcessorStarter());
+
+            PushBufferDataSource pbds = (PushBufferDataSource) processor.getDataOutput();
+            _engine.startRecognition(pbds, this);
+            processor.start();
+            _logger.debug("Performing recognition...");
+            _engine.startRecogThread();
+
+            // wait for result
+            RecognitionResult result = null;
+            synchronized (this) {
+                while (_result == null) {
+                    this.wait(1000);
+                }
+                result = _result;
+                _result = null;
+            }
+
+            _engine.passivate();
+
+            return result;
+        }
+
+        private Processor createReplicatedProcessor() throws IOException,
+                IllegalStateException, NoProcessorException,
+                CannotRealizeException {
+            
+            ProcessorModel pm = new ProcessorModel(
+                    _replicator.replicate(),
+                    PREFERRED_MEDIA_FORMATS,
+                    CONTENT_DESCRIPTOR_RAW
+            );
+            
+            _logger.debug("Creating realized processor...");
+            Processor processor = Manager.createRealizedProcessor(pm);
+            _logger.debug("Processor realized.");
+            
+            return processor;
+        }
+
+        private static Processor createMicrophoneProcessor()
+          throws NoDataSourceException, IOException, NoProcessorException, CannotRealizeException {
+
+            DataSource dataSource = Manager.createDataSource(MICROPHONE);
+            ProcessorModel pm = new ProcessorModel(dataSource,
+                    PREFERRED_MEDIA_FORMATS, CONTENT_DESCRIPTOR_RAW);
+            Processor processor = Manager.createRealizedProcessor(pm);
+            return processor;
+        }
+
+        private static PBDSReplicator createMicrophoneReplicator()
+          throws NoProcessorException, NoDataSourceException, CannotRealizeException, IOException {
+            Processor processor = createMicrophoneProcessor();
+            processor.addControllerListener(new ProcessorStarter());
+            PushBufferDataSource pbds = (PushBufferDataSource) processor.getDataOutput();
+            PBDSReplicator replicator = new PBDSReplicator(pbds);
+            processor.start();
+            return replicator;
+        }
+        
+        public static void main(String[] args) throws Exception {
+            URL url;
+            if (args.length > 0) {
+                url = new File(args[0]).toURL();
+            } else {
+                url = SphinxRecEngine.class.getResource("/config/sphinx-config.xml");
+            }
+            
+            if (url == null) {
+                throw new RuntimeException("Sphinx config file not found!");
+            }
+
+            System.out.println("Loading...");
+            ConfigurationManager cm = new ConfigurationManager(url);
+            SphinxRecEngine engine = new SphinxRecEngine(cm);
+
+            for (int i=0; i < 12; i++) {
+                System.out.println(engine._jsgfGrammar.getRandomSentence());
+            }
+            Test test = new Test(engine);
+            
+
+            RecognitionResult result;
+            while (true) {
+                result = test.doRecognize();
+            }
+
+//            RuleParse ruleParse = engine.parse("", "main");
+
+
+            //System.exit(0);
         }
 
     }
