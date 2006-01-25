b921ab08ac1910467247541a4b40d8ed62319fec
ngodfredsen <ngodfredsen@2108db3d-1710-0410-b8b3-d782175cc310>
2006-01-25 19:53:21 +0000

    update CLASSPATH to use jars in local Maven repository

--- trunk/cairo/bat/recenginetest.bat
+++ trunk/cairo/bat/recenginetest.bat
@@ -1,31 +1,25 @@
 @echo off
-set SOURCE_DIR=..
-set THIRDPARTY=%SOURCE_DIR%\..\..\thirdparty
-set JAVA_HOME=C:\java\jdk1.5.0
-set SPHINX_HOME=C:\sphinx\sphinx4-1.0beta
+echo ***************************************
+echo *  Make sure JAVA_HOME has been set!  *
+echo ***************************************
+
+set CAIRO_VERSION=SNAPSHOT
+set MVN_REPOSITORY=%USERPROFILE%\.m2\repository
 
 set CLASSPATH=.
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\gen\cairo.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\gen\mrcp4j.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\commons-collections-3.1.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\commons-configuration-1.1.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\commons-lang-2.1.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\commons-logging-1.0.4.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\commons-pool-1.2.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\JainSipApi1.1.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\jdom.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\jmf.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\log4j-1.2.12.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\mina-0.7.2.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\nist-sip-1.2.jar
-set CLASSPATH=%CLASSPATH%;%THIRDPARTY%\servlet-api.jar
-set CLASSPATH=%CLASSPATH%;%SPHINX_HOME%\lib\sphinx4.jar
-set CLASSPATH=%CLASSPATH%;%SPHINX_HOME%\lib\jsapi.jar
-set CLASSPATH=%CLASSPATH%;%SPHINX_HOME%\lib\WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz.jar
+set CLASSPATH=%CLASSPATH%;%MVN_REPOSITORY%\com\onomatopia\cairo\%CAIRO_VERSION%\cairo-%CAIRO_VERSION%.jar
+set CLASSPATH=%CLASSPATH%;%MVN_REPOSITORY%\org\mrcp4j\mrcp4j\0.1\mrcp4j-0.1.jar
+set CLASSPATH=%CLASSPATH%;%MVN_REPOSITORY%\commons-collections\commons-collections\3.1\commons-collections-3.1.jar
+set CLASSPATH=%CLASSPATH%;%MVN_REPOSITORY%\commons-configuration\commons-configuration\1.2\commons-configuration-1.2.jar
+set CLASSPATH=%CLASSPATH%;%MVN_REPOSITORY%\commons-lang\commons-lang\2.1\commons-lang-2.1.jar
+set CLASSPATH=%CLASSPATH%;%MVN_REPOSITORY%\commons-pool\commons-pool\1.2\commons-pool-1.2.jar
+set CLASSPATH=%CLASSPATH%;%MVN_REPOSITORY%\jdom\jdom\1.0\jdom-1.0.jar
+set CLASSPATH=%CLASSPATH%;%MVN_REPOSITORY%\log4j\log4j\1.2.13\log4j-1.2.13.jar
+set CLASSPATH=%CLASSPATH%;%MVN_REPOSITORY%\directory-network\mina\mina-0.7.2.jar
+set CLASSPATH=%CLASSPATH%;%MVN_REPOSITORY%\sphinx\sphinx4\1.0beta\sphinx4-1.0beta.jar
+set CLASSPATH=%CLASSPATH%;%MVN_REPOSITORY%\sphinx\WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz\1.0beta\WSJ_8gau_13dCep_16k_40mel_130Hz_6800Hz-1.0beta.jar
 
 set CLASS=com.onomatopia.cairo.server.recog.sphinx.SphinxRecEngine$Test
-set CAIRO_CONFIG=file:///C:/cvs/sf/cairo/config/cairo-config.xml
-set RES_NAME=input
 
-java -cp %CLASSPATH% -Xmx200m -Dlog4j.configuration=log4j.xml %CLASS%
+%JAVA_HOME%\bin\java -cp "%CLASSPATH%" -Xmx200m -Dlog4j.configuration=log4j.xml %CLASS%
 pause
